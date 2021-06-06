0b6c47b5517b09e289dec125f92af8a73605d26a
Janani Velmurugan <janani@hexaware.com>
2021-06-06 07:40:01 +0530

    added exercise

---
+++ README.md
@@ -0,0 +1,3 @@
+Exercises
+1. Create a calculator class to perform addition, subtraction, multiplication and division of two integers. Get inputs from command line/program arguments.
+
---
+++ training/src/Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+	public static void main(String[] args) {
+		System.out.println("Arithmetic Calculator");
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println("Addition is "+ a + b);
+	
+	}
+
+}
