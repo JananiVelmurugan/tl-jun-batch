e786acc331b3f0992d402c7fd95940cbd17794ab
Janani Velmurugan <janani@hexaware.com>
2021-08-21 13:27:30 +0530

    updated

--- sts/spring-boot-demo/src/main/java/com/training/springbootdemo/user/User.java
+++ sts/spring-boot-demo/src/main/java/com/training/springbootdemo/user/User.java
@@ -1,14 +1,14 @@
 package com.training.springbootdemo.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
 	@Id
+	@Column(name = "user_id")
 	int id;
 	String name;
 	String email;
--- sts/spring-boot-demo/src/main/java/com/training/springbootdemo/user/UserRepository.java
+++ sts/spring-boot-demo/src/main/java/com/training/springbootdemo/user/UserRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+	void print();
+
 }
